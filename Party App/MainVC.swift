8d6cc0dd71734e15a023c9f5a6d6aa6c46df6f56
Vasil Nechev <vnechev@Vasils-MacBook.local>
2016-10-14 12:41:06 +0300

    UI Created

---
+++ Party App/MainVC.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Party App
+//
+//  Created by Vasil Nechev on 10/13/16.
+//  Copyright © 2016 Vasil Nechev. All rights reserved.
+//
+
+import UIKit
+
+class MainVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+   
+
+}
+
